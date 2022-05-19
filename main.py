d5cade43e1191c50fe1f5fe51dcde059e00f6c4a
Peter Stone-Thompson <peter.stone-thompson@cybergrx.com>
2022-05-19 19:05:24 +0100

    Add ability to read exercise input

---
+++ main.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+def get_exercise_lines() -> List[str]:
+    # https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
+    with open("exercise_part_one_input.txt", 'r') as f:
+        read_data = f.read()
+
+    file_lines = read_data.split("\n")
+
+    return file_lines
+
+
+if __name__ == '__main__':
+    exercise_lines = get_exercise_lines()
